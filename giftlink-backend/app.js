6f05657c5ebdf22f8c029956598750afb5b2d589
Vihang Karekar <vihang.karekar@gmail.com>
2024-09-22 02:41:35 -0400

    Create API for Search Service

--- giftlink-backend/app.js
+++ giftlink-backend/app.js
@@ -26,7 +26,7 @@ app.use(express.json());
 const giftRoutes = require('./routes/giftRoutes');
 
 // Search API Task 1: import the searchRoutes and store in a constant called searchRoutes
-// const searchRoutes = require('./routes/searchRoutes');
+const searchRoutes = require('./routes/searchRoutes');
 
 
 const pinoHttp = require('pino-http');
@@ -39,7 +39,7 @@ app.use(pinoHttp({ logger }));
 app.use('/api/gifts', giftRoutes);
 
 // Search API Task 2: add the searchRoutes to the server by using the app.use() method.
-//{{insert code here}}
+app.use('/api/search', searchRoutes);
 
 
 // Global Error Handler
