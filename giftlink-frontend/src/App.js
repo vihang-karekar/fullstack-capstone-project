c3696d72f04886e8aa38e8ece18a5077fc66a331
Vihang Karekar <vihang.karekar@gmail.com>
2024-09-30 10:16:20 -0400

    Create the Register and Login Pages

--- giftlink-frontend/src/App.js
+++ giftlink-frontend/src/App.js
@@ -4,9 +4,11 @@ import MainPage from './components/MainPage/MainPage';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
+import RegisterPage from './components/RegisterPage/RegisterPage';
+import LoginPage from './components/LoginPage/LoginPage';
 
 function App() {
-
+const navigate = useNavigate();
   return (
     <>
         <Navbar/>
@@ -14,6 +16,8 @@ function App() {
           {/* the final code will not pass the products to every page, but each page will call the server API */}
           <Route path="/" element={<MainPage />} />
           <Route path="/app" element={<MainPage />} />
+          <Route path="/app/login" element={<LoginPage/>} />
+          <Route path="/app/register" element={<RegisterPage />} />
         </Routes>
         </>
   );
