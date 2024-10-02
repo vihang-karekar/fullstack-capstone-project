d71275b43d72c2b5ef109671a48549b6ec15cbcc
Vihang Karekar <vihang.karekar@gmail.com>
2024-10-02 03:55:49 -0400

    Implement Backend in Login Component

--- giftlink-frontend/src/components/LoginPage/LoginPage.js
+++ giftlink-frontend/src/components/LoginPage/LoginPage.js
@@ -1,4 +1,12 @@
 import React, { useState } from 'react';
+//Task 1: Import urlConfig from `giftlink-frontend/src/config.js`
+import {urlConfig} from '../../config';
+
+//Task 2: Import useAppContext `giftlink-frontend/context/AuthContext.js`
+import { useAppContext } from '../../context/AuthContext';
+
+//Task 3: Import useNavigate from `react-router-dom` to handle navigation after successful registration.
+import { useNavigate } from 'react-router-dom';
 import './LoginPage.css';
 
 function LoginPage() {
@@ -6,11 +14,64 @@ function LoginPage() {
     //insert code here to create useState hook variables for email, password
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+    //Task 4: Include a state for incorrect password.
+    const [incorrect, setIncorrect] = useState('');
+
+    //Task 5: Create a local variable for `navigate`,`bearerToken`   and `setIsLoggedIn`.
+    const navigate = useNavigate();
+    const bearerToken = sessionStorage.getItem('bearer-token');
+    const { setIsLoggedIn } = useAppContext();
+
+    //Task 6. If the bearerToken has a value (user already logged in), navigate to MainPage
+    useEffect(() => {
+        if (sessionStorage.getItem('auth-token')) {
+          navigate('/app')
+        }
+      }, [navigate])
 
     // insert code here to create handleLogin function and include console.log
-    const handleLogin = async () => {
+    const handleLogin = async (e) => {
+        e.preventDefault();
         console.log("login invoked")
+            const response = await fetch(`${urlConfig.backendUrl}/api/auth/login`, {
+                 //Task 7: Set method
+                 method: 'POST',
+                 //Task 8: Set headers
+                 headers: {
+                    'content-type': 'application/json',
+                    'Authorization': bearerToken ? `Bearer ${bearerToken}` : '', // Include Bearer token if available
+                  },        
+                  body: JSON.stringify({    
+                    email: email,
+                    password: password,
+                  })
+            })
+
+             // Task 1: Access data coming from fetch API
+             const json = await response.json();
+           console.log('json data', json);
+
+            // Task 2: Set user details
+            if (json.authtoken) {
+                sessionStorage.setItem('auth-token', json.authtoken);
+                sessionStorage.setItem('name', json.userName);
+                sessionStorage.setItem('email', json.userEmail);
+                // Task 3: Set the user's state to log in using the `useAppContext`.
+                setIsLoggedIn(true);
+                // Task 4: Navigate to the MainPage after logging in.
+                navigate('/app');
+            } else {
+                // Task 5: Clear input and set an error message if the password is incorrect
+                document.getElementById("email").value="";
+                document.getElementById("password").value="";
+                setIncorrect("Wrong password. Try again.");
+            //Below is optional, but recommended - Clear out error message after 2 seconds
+                setTimeout(() => {
+                  setIncorrect("");
+                }, 2000);
+              }
     }
+    
         return (
       <div className="container mt-5">
         <div className="row justify-content-center">
@@ -28,7 +89,7 @@ function LoginPage() {
                             className="form-control"
                             placeholder="Enter your email"
                             value={email}
-                            onChange={(e) => setEmail(e.target.value)}
+                            onChange={(e) => {setEmail(e.target.value); setIncorrect("")}}
                         />
                     </div>
                     {/* password */}
@@ -40,8 +101,10 @@ function LoginPage() {
                             className="form-control"
                             placeholder="Enter your password"
                             value={password}
-                            onChange={(e) => setPassword(e.target.value)}
+                            onChange={(e) => {setPassword(e.target.value);setIncorrect("")}}
                         />
+                     {/*Task 6: Display an error message to the user.*/}
+                    <span style={{color:'red',height:'.5cm',display:'block',fontStyle:'italic',fontSize:'12px'}}>{incorrect}</span>
                     </div>
 
           {/* insert code here to create a button that performs the `handleLogin` function on click */}
